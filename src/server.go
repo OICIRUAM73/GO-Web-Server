b84d1f67c8c5528b2e36327805380ab5d17c6476
Mauricio <oiciruam73@hotmail.com>
2020-04-19 17:09:46 -0500

    Create server

---
+++ src/server.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+type Server struct {
+	port string
+}
+
+func NewServer(port string) *Server {
+	return &Server{
+		port: port,
+	}
+}
+
+func (s *Server) Listen() error {
+	err := http.ListenAndServe(s.port, nil)
+	return err
+}
